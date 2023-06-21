b1fee5251c1d7ee3e351c0fb02b4d26eb74b533e
息占博 <xizhanbo@huatu.com>
2023-06-21 11:37:32 +0800

    f

--- algorithm/中位数.go
+++ algorithm/中位数.go
@@ -5,25 +5,40 @@
 // 	解释：合并数组 = [1,2,3] ，中位数 2
 // 示例 2：
 
-// 	输入：nums1 = [1,2], nums2 = [3,4]
-// 	输出：2.50000
-// 	解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+// 输入：nums1 = [1,2], nums2 = [3,4]
+// 输出：2.50000
+// 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
 package main
 
-func find(num1, num2 []int) float64 {
+import (
+	"fmt"
+	"strconv"
+)
+
+// Comparable 代表可以进行比较的int和float类型
+type Comparable[T any] interface {
+	int | float64
+}
+
+func find(num1, num2 []int) interface{} {
 	l1 := len(num1)
 	l2 := len(num2)
 	sum := l1 + l2
-	if sum%2 == 1 { //奇数去中间
-		return float64(getNumByIndex(num1, num2, sum/2))
-	} else {
 
-		return (float64(getNumByIndex(num1, num2, sum/2-1)) + float64(getNumByIndex(num1, num2, sum/2))) / 2.0
+	if sum%2 == 1 { //奇数取中间
+		result := getNumByIndex(num1, num2, sum/2)
+		return result
+	} else {
+		add := float64(getNumByIndex(num1, num2, sum/2-1)) + float64(getNumByIndex(num1, num2, sum/2))
+		result := fmt.Sprintf("%.1f", add/2.0)
+		res, _ := strconv.ParseFloat(result, 64)
+		return res
 	}
 }
 
+// 该方法默认是升序排列了
 func getNumByIndex(x, y []int, index int) int {
-	i, x1, y1 := 0, 0, 0
+	i, x1, y1 := 0, 0, 0 //i 代表循环次数，x1/y1分别代表两个数组的当前索引位置
 	res := 0
 	l1 := len(x)
 	l2 := len(y)
@@ -55,9 +70,16 @@ func getNumByIndex(x, y []int, index int) int {
 
 func main() {
 	x := []int{1, 3}
-	y := []int{2}
-	println(find(x, y))
-	// x2 := []int{1, 2}
-	// y2 := []int{3, 4}
-	// println(find(x2, y2))
+	y := []int{2, 5}
+	res := find(x, y)
+	fmt.Println(res)
+	//v := reflect.ValueOf(res)
+	//switch v.Kind() {
+	//case reflect.Int:
+	//	i := v.Int()
+	//	fmt.Println(i)
+	//case reflect.Float64:
+	//	f := v.Float()
+	//	fmt.Printf("%.1f\n", f)
+	//}
 }
